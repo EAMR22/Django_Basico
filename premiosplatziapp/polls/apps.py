26a9a293da299f7c02ef45d4c57e393d951e09fa
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-06-20 23:25:34 -0600

    Haciendo migraciones del proyecto

--- premiosplatziapp/polls/apps.py
+++ premiosplatziapp/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
+class PollsConfig(AppConfig):     # Hace que nuestra aplicacion funcione dentro del proyecto.
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'polls'
--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,3 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):    # En django no es necesario generar un "id" porque automaticamente tiene un incrementador por dentro.
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)   # En on_delete cuando volvemos a la pregunta, borramos en cascada el resto de las preguntas.
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
