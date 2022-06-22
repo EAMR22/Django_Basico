5c6a1e2883c2f1961ff92ad67bd43668ec9f9edc
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-06-21 22:00:03 -0600

    Manejando la consola interactiva de django

--- premiosplatziapp/polls/models.py
+++ premiosplatziapp/polls/models.py
@@ -1,11 +1,24 @@
+import datetime
+
 from tkinter import CASCADE
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):    # En django no es necesario generar un "id" porque automaticamente tiene un incrementador por dentro.
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)   # En on_delete cuando volvemos a la pregunta, borramos en cascada el resto de las preguntas.
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
