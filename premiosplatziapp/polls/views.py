2963aaf45a2c1a3e934024879fd126080d4b57df
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-06-23 22:09:21 -0600

    Desarrollando los templates y el formulario del proyecto

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Estas en la pagina principal de premios platzi app")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    }) # render lleva 3 parametros: el 1 la respuesta, el 2 la ubicacion del template, el 3 un objeto con las variables usadas por el template.
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id) 
+    return render(request, "polls/detail.html", {
+        "question": question
+    }) # El error 404 lleva 2 parametros: EL 1 el modelo y el 2 el valor a ejecutar internamente en la funcion get.
 
 
 def results(request, question_id):
